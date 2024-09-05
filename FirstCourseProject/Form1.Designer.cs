7e0ac89cd0b8acfdfeae6ab39ea0e36f9efe619f
s4vagg <v.s.zhirenkov@yandex.ru>
2024-09-05 14:39:49 +0300

    first

---
+++ FirstCourseProject/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace HillCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(286, 381);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(310, 28);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(286, 381);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(86, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Сохранить зашифрованный текст";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Поле для шифрования";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(307, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Поле для дешифрования";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(398, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 39);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Шифровать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(495, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 39);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Расшифровать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(250, 415);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(142, 39);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Сохранить расшифрованный текст";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 471);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(409, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 478);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ключ";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(497, 460);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 39);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Очистить всё";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 503);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Курсовая работа";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ FirstCourseProject/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HillCipher
+{
+    public partial class Form1 : Form
+    {
+        string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Переменная алфавита
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Очистка всех полей
+        private void button6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            textBox1.Clear();
+        }
+
+        // Открытие файла с текстом
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Текстовые документы (*.txt)|*.txt";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                DialogResult dialogResult = MessageBox.Show("Вы открываете зашифрованный текст?", "Выберите действие", MessageBoxButtons.YesNo);
+
+                using (StreamReader sr = new StreamReader(ofd.FileName))
+                {
+                    string text = sr.ReadToEnd();
+
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        richTextBox2.Clear();
+                        richTextBox2.Text = text;
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        richTextBox1.Clear();
+                        richTextBox1.Text = text;
+                    }
+                }
+            }
+        }
+
+        // Сохранение зашифрованного текста в файл
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые документы (*.txt)|*.txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName))
+                {
+                    sw.Write(richTextBox2.Text);
+                }
+            }
+        }
+
+        // Сохранение расшифрованного текста в файл
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые документы (*.txt)|*.txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName))
+                {
+                    sw.Write(richTextBox1.Text);
+                }
+            }
+        }
+
+        // Шифрование текста
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string text = richTextBox1.Text;
+            string key = textBox1.Text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                MessageBox.Show("Введите шифруемый текст", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(key))
+            {
+                MessageBox.Show("Введите ключ", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                string encrypted = EncryptText(text, key);
+                richTextBox2.Text = encrypted;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при шифровании: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Расшифрование текста
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string text = richTextBox2.Text;
+            string key = textBox1.Text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                MessageBox.Show("Введите текст для расшифровки", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(key))
+            {
+                MessageBox.Show("Введите ключ", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                string decrypted = DecryptText(text, key);
+                if (decrypted != null)
+                    richTextBox1.Text = decrypted;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при расшифровке: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Метод для шифрования текста
+        private string EncryptText(string plainText, string key)
+        {
+            string encText = Encrypt(plainText.ToUpper(), key.ToUpper());
+            return encText;
+        }
+
+        // Метод для расшифрования текста
+        private string DecryptText(string encryptedText, string key)
+        {
+            string decText = Decrypt(encryptedText.ToUpper(), key.ToUpper());
+            return decText;
+        }
+
+        // Метод для шифрования текста с помощью алгоритма Hill Cipher
+        private string Encrypt(string textToEncrypt, string key)
+        {
+            string encryptedText = "";
+            int[,] matrix = GetMatrixFromText(key);
+            string text = CleanText(textToEncrypt);
+
+            // Убедимся, что длина текста кратна 2 для корректной работы шифрования
+            if (text.Length % 2 != 0)
+            {
+                text += 'X'; // Добавляем дополнительный символ, чтобы сделать длину текста кратной 2
+            }
+
+            for (int i = 0; i < text.Length; i += 2)
+            {
+                int numOne = alphabet.IndexOf(text[i]);
+                int numTwo = alphabet.IndexOf(text[i + 1]);
+                int firstProduct = matrix[0, 0] * numOne + matrix[0, 1] * numTwo;
+                int secondProduct = matrix[1, 0] * numOne + matrix[1, 1] * numTwo;
+                encryptedText += alphabet[(firstProduct % 26 + 26) % 26]; // Исправляем вычисление модуля
+                encryptedText += alphabet[(secondProduct % 26 + 26) % 26]; // Исправляем вычисление модуля
+            }
+            return encryptedText;
+        }
+
+        // Метод для расшифрования текста с помощью алгоритма Hill Cipher
+        private string Decrypt(string textToDecrypt, string key)
+        {
+            string decryptedText = "";
+            int[,] inputMatrix = GetMatrixFromText(key);
+            double[,] matrix = InverseMatrix(inputMatrix);
+            string text = textToDecrypt;
+
+            for (int i = 0; i < text.Length; i = i + 2)
+            {
+                int numOne = alphabet.IndexOf(text[i]);
+                int numTwo = alphabet.IndexOf(text[i + 1]);
+                double firstProduct = matrix[0, 0] * numOne + matrix[0, 1] * numTwo;
+                double secondProduct = matrix[1, 0] * numOne + matrix[1, 1] * numTwo;
+                int firstRounded = Convert.ToInt32(Math.Floor(firstProduct) % 26);
+                int secondRounded = Convert.ToInt32(Math.Floor(secondProduct) % 26);
+
+                decryptedText += alphabet[(firstRounded + 26) % 26]; // Исправляем вычисление модуля
+                decryptedText += alphabet[(secondRounded + 26) % 26]; // Исправляем вычисление модуля
+            }
+
+            return decryptedText;
+        }
+
+        // Метод для вычисления обратной матрицы
+        private double[,] InverseMatrix(int[,] matrix)
+        {
+            double[,] inverse = new double[2, 2];
+
+            int determinant = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            if (determinant < 0)
+            {
+                determinant = determinant * -1;
+            }
+            double detFraction = ModInverse(determinant, 26);
+            inverse[0, 0] = (matrix[1, 1] * detFraction) % 26;
+            inverse[0, 1] = ((-matrix[0, 1] + 26) * detFraction) % 26;
+            inverse[1, 0] = ((-matrix[1, 0] + 26) * detFraction) % 26;
+            inverse[1, 1] = (matrix[0, 0] * detFraction) % 26;
+
+            return inverse;
+        }
+
+        // Метод для вычисления модуля
+        private int ModInverse(int a, int n)
+        {
+            int i = n, v = 0, d = 1;
+            while (a > 0)
+            {
+                int t = i / a, x = a;
+                a = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= n;
+            if (v < 0) v = (v + n) % n;
+            return v;
+        }
+
+        // Метод для получения матрицы из ключа
+        private int[,] GetMatrixFromText(string text)
+        {
+            int[,] matrix = new int[2, 2];
+            int textCounter = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    matrix[i, j] = alphabet.IndexOf(text[textCounter]);
+                    textCounter++;
+                }
+            }
+            return matrix;
+        }
+
+        // Метод для очистки текста от символов, не входящих в алфавит
+        private string CleanText(string text)
+        {
+            string cleanedText = "";
+            foreach (char c in text)
+            {
+                if (alphabet.Contains(c.ToString()))
+                {
+                    cleanedText += c;
+                }
+            }
+            return cleanedText;
+        }
+    }
+}
